f93cb47e1ea94a55f84e8d0c777f07f65e3fd137
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-02-19 17:02:07 -0300

    Condicional para Postagens

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -1,11 +1,17 @@
 ﻿import { Link } from 'react-router-dom'
 import Postagem from '../../../models/Postagem'
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 interface CardPostagensProps {
     postagem: Postagem // Indicamos que a prop postagem é um Objeto (do Tipo/da Model) Postagem
 }
 
 function CardPostagem({ postagem }: CardPostagensProps) {   // Passamos a props postagem para o card
+
+    const { usuario } = useContext(AuthContext);
+
+
     return (
         <div className='border-slate-900 border 
             flex flex-col rounded overflow-hidden justify-between'>
@@ -30,18 +36,24 @@ function CardPostagem({ postagem }: CardPostagensProps) {   // Passamos a props 
                     }).format(new Date(postagem.data))}</p>
                 </div>
             </div>
-            <div className="flex">
-                <Link to={`/editarpostagem/${postagem.id}`}
-                    className='w-full text-white bg-indigo-400 
-                    hover:bg-indigo-800 flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
-                <Link to={`/deletarpostagem/${postagem.id}`}
-                    className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
-            </div>
+
+            {
+                postagem.usuario?.id === usuario.id &&
+                (
+                    <div className="flex">
+                        <Link to={`/editarpostagem/${postagem.id}`}
+                            className='w-full text-white bg-indigo-400 
+                        hover:bg-indigo-800 flex items-center justify-center py-2'>
+                            <button>Editar</button>
+                        </Link>
+                        <Link to={`/deletarpostagem/${postagem.id}`}
+                            className='text-white bg-red-400 
+                        hover:bg-red-700 w-full flex items-center justify-center'>
+                            <button>Deletar</button>
+                        </Link>
+                    </div>
+                )
+            }
         </div>
     )
 }
